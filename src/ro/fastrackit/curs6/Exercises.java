53dbfc2e785c45061ae13d7d404d878d9dbb0be2
Adrian Buzamat <adrian.buzamat@yahoo.com>
2021-10-20 15:55:37 +0300

    Updated with exercises 4->6

--- src/ro/fastrackit/curs6/Exercises.java
+++ src/ro/fastrackit/curs6/Exercises.java
@@ -1,6 +1,7 @@
 package ro.fastrackit.curs6;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Exercises {
     public static void main(String[] args) {
@@ -8,6 +9,9 @@ public class Exercises {
         integersSum(intNumbers);
         oddNumbers(intNumbers);
         biggerThanGivenNr(intNumbers, 5);
+        donations(25000);
+        limitedDonations(25000, 10);
+        textToSplit("One morning Josef Kahn is arrested for a crime he did not commit. In fact, he is never even told the nature of the offence. His life is thrown into turmoil as he becomes enmeshed in a struggle to prove his innocence. As the confounding case unfolds, Kahn is ultimately powerless - battling against a remote and uncaring bureaucracy.");
 
 
     }
@@ -45,6 +49,39 @@ public class Exercises {
             }
         }
         return bigger;
+    }
+
+    static void donations(int target) {
+        int donated = 0;
+        do {
+            Random random = new Random();
+            int rand = random.nextInt(2000);
+            donated += rand;
+
+        } while (donated <= target);
+
+        System.out.println(donated);
+    }
+
+    static void limitedDonations(int target, int maxDonation) {
+        int donated = 0;
+        int count = 0;
+        for (int i = 1; i <= maxDonation; i++) {
+            count++;
+            if (count <= maxDonation & donated <= target) {
+                Random random = new Random();
+                int rand = random.nextInt(2000);
+                donated += rand;
+            }
+        }
+        System.out.println(donated);
+    }
+
+    static void textToSplit(String text){
+        String[] arrOfText = text.split("\\.");
+        for (String a: arrOfText){
+            System.out.println(a.trim());
+        }
     }
 
 }
